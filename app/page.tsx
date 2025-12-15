9833cff76545635468b64bf14c0fdb29eabb584a
andersthemagi <andres@redmage.cc>
2025-12-15 01:47:59 +0000

    feat: added Quotebox component for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import { logout } from "./(auth)/actions";
 
-export default function Home() {
+import { createClient } from "@/lib/supabase/server";
+import { logout } from "./(auth)/actions";
+import QuoteBox from "./components/ui/QuoteBox";
+
+const DEFAULT_QUOTE = "That brain of mine is something more than merely mortal; as time will show."
+const DEFAULT_ATTRIBUTION = "Ada Lovelace, Countess"
+
+export default async function Home() {
+
+  const supabase = await createClient();
+  
+  const { data : { user }, } = await supabase.auth.getUser();
+
   return (
-    <div>
-      <h1>Welcome to Vigri</h1>
+    <div className="mx-auto">
+      <h1>Welcome Back, $$USER$$</h1>
+      <QuoteBox quote={DEFAULT_QUOTE} attribution={DEFAULT_ATTRIBUTION} />
     </div>
   );
 }
