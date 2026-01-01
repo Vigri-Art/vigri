7ad8aea4334aef4f45c51e05127cc91e5f8d3e40
Andres Sepulveda Morales <andres@redmage.cc>
2026-01-01 14:05:00 -0700

    style: updated max width of login card

--- app/components/ui/NavWrapper.tsx
+++ app/components/ui/NavWrapper.tsx
@@ -25,7 +25,7 @@ export function NavWrapper({ children, user }: NavWrapperProps) {
     return (
       <div className="min-h-screen">
         <main className="flex px-4 h-screen align-middle"> 
-          <div className="lg:w-2/3 m:w-1/2 sm:w-2/3 m-auto grid grid-cols-1 lg:grid-cols-12 gap-6 pt-8 lg:pt-8"> 
+          <div className="lg:w-2/3 m:w-1/2 sm:w-2/3 m-auto max-w-lg grid grid-cols-1 lg:grid-cols-12 gap-6 pt-8 lg:pt-8"> 
             <div className="lg:col-span-12">
               {children}
             </div>
